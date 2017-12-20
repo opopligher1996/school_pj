16f48e69c7790889ae7115842b0d9a75b1c97370
opopligher1996 <opopligher1996@gmail.com>
2017-12-20 23:10:20 +0800

    first

---
+++ testphp.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+</head>
+
+<body>
+    <?php 
+ $test='Hello World';
+ echo $test;
+?>
+</body>
+</html>
